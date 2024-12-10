7bf4f304aa1f5dedca17d7c71fe1c0b6f1594289
Luki248 <lukas.meier@katta.ch>
2024-12-10 21:27:12 +0100

    solved day 7 first puzzle

--- Day_07/AoC_day7.py
+++ Day_07/AoC_day7.py
@@ -17,20 +17,33 @@ for line in input:
         nums_int.append(int(num))
     numbers.append(nums_int)
 
-def calc_res(nums, operator):
+def calc_res(nums):
     if len(nums) < 3:
-        if operator == "+":
-            return nums[0] + nums[1]
-        else:
-            return nums[0] * nums[1]
+        return nums[0] + nums[1], nums[0] * nums[1]
     else:
-        if operator == "+":
-            return nums[0] + calc_res(nums[1:], "+")
-        else:
-            return nums[0] * calc_res(nums[1:], "+")
+        ret = calc_res(nums[0:-1])
+        calculations = []
+        for res in ret:
+            calculations.append(nums[-1] + res)
+            calculations.append(nums[-1] * res)
+        return calculations
 
-print(calc_res(numbers[1], "+"))
-print("First Puzzle:")
+is_eq_correct = []
+for i in range(len(numbers)):
+    number_of_results = pow(2, len(numbers[i]) - 1)
+    eq_results = calc_res(numbers[i])
+
+    is_equation_correct = False
+    for res in eq_results:
+        if res == results[i]:
+            is_equation_correct = True
+    is_eq_correct.append(is_equation_correct)
+
+sum = 0
+for i in range(len(is_eq_correct)):
+    if is_eq_correct[i]:
+        sum += results[i]
+print("First Puzzle:", sum)
 
 
 print("Second Puzzle:")
