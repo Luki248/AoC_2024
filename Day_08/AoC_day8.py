932100300e96eb51fb2c61ce5df575ac4ca5083c
Luki248 <lukas.meier@katta.ch>
2024-12-14 19:22:13 +0100

    trying

--- Day_08/AoC_day8.py
+++ Day_08/AoC_day8.py
@@ -19,6 +19,36 @@ for i in range(len(grid[0])):
         if grid[i][j] != ".":
             antennas.append([grid[i][j], i, j])
 
+total_antinodes = 0
+for ant1 in antennas:
+    print(i)
+
+    for ant2 in antennas:
+        if ant1 == ant2:
+            continue
+        if ant1[0] == ant2[0]:
+            delta_y = abs(ant1[1] - ant2[1])
+            if ant1[1] > ant2[1]:
+                antinode_y1 = ant1[1] + delta_y
+                antinode_y2 = ant2[1] - delta_y
+            else:
+                antinode_y2 = ant1[1] + delta_y
+                antinode_y1 = ant2[1] - delta_y
+            delta_x = abs(ant1[2] - ant2[2])
+            if ant1[2] > ant2[2]:
+                antinode_x1 = ant1[2] + delta_x
+                antinode_x2 = ant2[2] - delta_x
+            else:
+                antinode_x1 = ant1[2] + delta_x
+                antinode_x2 = ant2[2] - delta_x
+
+            if antinode_x1 >= 0 and antinode_x1 < len(grid[0]):
+                if antinode_y1 >= 0 and antinode_y1 < len(grid):
+                    total_antinodes += 1
+            if antinode_x2 >= 0 and antinode_x2 < len(grid[0]):
+                if antinode_y2 >= 0 and antinode_y2 < len(grid):
+                    total_antinodes += 1
+
 print("First Puzzle:")
 
 print("Second Puzzle:")
