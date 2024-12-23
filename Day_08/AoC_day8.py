0b47618e121adbd78fbeb8fcfaf1e391d6549f47
Luki248 <lukas.meier@katta.ch>
2024-12-23 14:28:02 +0100

    trying

--- Day_08/AoC_day8.py
+++ Day_08/AoC_day8.py
@@ -20,9 +20,9 @@ for i in range(len(grid[0])):
             antennas.append([grid[i][j], i, j])
 
 total_antinodes = 0
+antinodes = []
 for ant1 in antennas:
-    print(i)
-
+    antennas.remove(antennas[0])
     for ant2 in antennas:
         if ant1 == ant2:
             continue
@@ -32,8 +32,8 @@ for ant1 in antennas:
                 antinode_y1 = ant1[1] + delta_y
                 antinode_y2 = ant2[1] - delta_y
             else:
-                antinode_y2 = ant1[1] + delta_y
-                antinode_y1 = ant2[1] - delta_y
+                antinode_y2 = ant2[1] + delta_y
+                antinode_y1 = ant1[1] - delta_y
             delta_x = abs(ant1[2] - ant2[2])
             if ant1[2] > ant2[2]:
                 antinode_x1 = ant1[2] + delta_x
@@ -45,9 +45,19 @@ for ant1 in antennas:
             if antinode_x1 >= 0 and antinode_x1 < len(grid[0]):
                 if antinode_y1 >= 0 and antinode_y1 < len(grid):
                     total_antinodes += 1
+                    antinodes.append([antinode_x1, antinode_y1])
+                    grid[antinode_x1][antinode_y1] = "#"
             if antinode_x2 >= 0 and antinode_x2 < len(grid[0]):
                 if antinode_y2 >= 0 and antinode_y2 < len(grid):
                     total_antinodes += 1
+                    antinodes.append([antinode_x2, antinode_y2])
+                    grid[antinode_x2][antinode_y2] = "#"
+antinodes.sort()
+
+for i in range(len(grid[0])):
+    for j in range(len(grid)):
+        print(grid[i][j], end="")
+    print("")
 
 print("First Puzzle:")
 
