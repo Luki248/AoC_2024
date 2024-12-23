d6609c1cfc7b0fc3c0719a35fb21397bd93c5cb8
Luki248 <lukas.meier@katta.ch>
2024-12-23 22:49:18 +0100

    trying day 9, second puzzle

--- Day_09/AoC_day9.py
+++ Day_09/AoC_day9.py
@@ -47,9 +47,37 @@ for cell in memory:
     for data in cell[0]:
         memory3.append([data, cell[1]])
 
-for i in range(len(memory3) - 1, 0, -1):
-    if memory3[i][0] != ".":
+def move_data_if_possible(i):
+    data_len = 1
+    while memory3[i - data_len][0] == memory3[i][0]:
+        data_len += 1
 
+    for j in range(len(memory3)):
+        if j >= i:
+            return data_len
+        free_space_len = 1
+        if memory3[j][0] == ".":
+            if (j + free_space_len) < len(memory3):
+                while memory3[j + free_space_len][0] == ".":
+                    free_space_len += 1
+                    if (j + free_space_len) >= len(memory3):
+                        return data_len
+            else:
+                return data_len
+
+            if free_space_len >= data_len:
+                for l in range(data_len):
+                    memory3[j + l] = memory3[i - l]
+                    memory3[i - l] = [".", "."]
+                return data_len
+
+index = len(memory3) - 1
+while index > 0:
+    if memory3[index][0] != ".":
+        index -= move_data_if_possible(index)
+    else:
+        index -= 1
+    print("\r... {0:2.1f}%".format(100 - (index / len(memory3) * 100)), end="")
 
 checksum2 = 0
 i = 0
@@ -57,4 +85,4 @@ for cell in memory3:
     if cell[1] != ".":
         checksum2 += i * cell[1]
     i += 1
-print("Second Puzzle:", checksum2)
+print("\rSecond Puzzle:", checksum2)
